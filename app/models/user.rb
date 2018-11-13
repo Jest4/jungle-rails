c5044f6fd2b8b8fc39b1eb6c8170ef60511ed539
Jeff E <jeffreyestany.gb@gmail.com>
2018-11-13 00:47:04 +0000

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+
+  validates :password, confirmation: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, :first_name, :last_name, presence: true
+  validates :password, length: { in: 4..20 }
+
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+  end
+
 end
