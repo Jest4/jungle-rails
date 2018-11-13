651d67862ca616a9a852e7a50d88cf98ae1ccbea
Jeff E <jeffreyestany.gb@gmail.com>
2018-11-13 02:09:54 +0000

    refactored authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
+    # If the user exists AND the password entered is correct.
+    user = User.find_by_email(email)
+    user && user.authenticate(password)
   end
-
 end
